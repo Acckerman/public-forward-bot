00895d9466cc9f52b37ab55ec6e7cf234b276511
Acckerman <109450912+Acckerman@users.noreply.github.com>
2023-03-07 16:22:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "Forward")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Data')
     SESSION = os.environ.get("SESSION", "BQBhmScTP9n3QR-eVEC58ZFugKOuNYkSaXZYRDI248mpSmT5wePJKZH7wpBNy7fi7JxB7Zc3lmDkXza-VteO1itFQ0b-voD2fViNt0APl8sWrDqnU11BKynD9c2Wqvfj0k8hL_IuZPqJsFqH5LUMOy0M2t5EPU6ECnLW-OEWyLMYPxOhftHFrxIF2XPVqqd0tGgAsN08QNdeQ-GTNXuHv5QmyZIjy-k_GiL8RPcMdnzLZIoaL8QAT2-6g78S0cvSk2KPy55Ihke0PAd5V5PddxvGAg5fE2r_Pz7FNakOW3gY9P30eTn-RVQMQ1WF3r-sE5_t8i7qbR1dfkC8W9q_7ymBaIV0tQA")   
-    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001682757823"))
+    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001824133475"))
     BOT_USERNAME= os.environ.get("BOT_USERNAME", "")
 
 
